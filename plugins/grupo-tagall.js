7e325040759f03e0d8076d82bc6e8b2b6bdfdebd
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-06 13:20:53 -0400

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -8,14 +8,15 @@ let pesan = args.join` `
 let oi = `*𝙈𝙚𝙣𝙨𝙖𝙟𝙚:* ${pesan}`
 let teks = `╭┄〔 *${wm}* 〕┄⊱\n┆ 𝘼𝙘𝙩𝙞𝙫𝙚𝙣𝙨𝙚 𝙥𝙡𝙖𝙣𝙩𝙖𝙨 🙊*\n┆\n┆❏ ${oi}\n┆\n┆❏ *𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖𝙨:*\n`
 for (let mem of participants) {
-teks += `┆🙈 @${mem.id.split('@')[0]}\n`}
-teks += `└ @𝐦𝐨𝐧𝐤𝐢_𝐛𝐨𝐭 🐵 \n\n`
+teks += `┏━ *𝙻𝙾𝚂 𝙸𝙽𝚅𝙾𝙲𝙾 𝙱𝙾𝚃𝚂*🦁`
+teks += `┣🦁 @${mem.id.split('@')[0]}\n`}
+teks += `┗━ @𝐃𝐲𝐥𝐚𝐧𝐁𝐨𝐭 🦁 \n\n`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(tagall|invocar|invocacion|todos|plantas|invocación)$/i
 handler.admin = true
 handler.group = true
 export default handler
