099939dd1b5b1c0e1d5429e80eaae0d7a69049e0
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-06 13:15:13 -0400

    Actualizar plugins

---
+++ plugins/menú+18.js
@@ -0,0 +1,84 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+try {
+await m.reply(`╭「 𝐃𝐘𝐋𝐀𝐍 𝐁𝐎𝐓 」\n│➯(♦️)*Hola estás en el menú+18 elige una opción y pide una imagen*\n╰───────────────╯\n\n\n╭「 𝐃𝐘𝐋𝐀𝐍 𝐁𝐎𝐓 」\n│➯(♦️)*Hello you are in the menu+18 choose an option and ask for an image*\n╰───────────────╯`)
+let pp = imagen4
+let img = await(await fetch('https://i.imgur.com/JP52fdP.jpg')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let user = global.db.data.users[m.sender]
+let { money, joincount } = global.db.data.users[m.sender]
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `╭━━━━━━━━━━━━━━━━╾•
+┣▢ ❤𝐇𝐎𝐋𝐀, ${username}❤
+┣━━━━━━━━━━━━━━━━╾• 
+┃  ❍ (𝐌𝐄𝐍𝐔🎶) ❍* 
+┣━━━━━━━━━━━━━━━━╾•  
+┃⋄➯𝗛ᴏʟᴀ,➟${taguser}
+┃⋄➯ *𝙲𝚁𝙴𝙰𝙳𝙾𝚁:Karim.dzn
+┃⋄➯ *𝙸𝙽𝙵𝙾: Wa.me/59176181985
+╰━━━━━━━━━━━━━━━━╾• 
+🍁᭢━━━━━━━━━᭥🍁᭢
+╭「 𝐃𝐘𝐋𝐀𝐍 𝐁𝐎𝐓 」
+│➯*Menu*+18*
+︎╰───────────────╯
+🍁᭢━━━━━━━━━᭥🍁᭢
+╭─「➻❥ *Hentai+* 18➻❥」
+│➯ *.nsfwloli*
+│➯ *.nsfwfoot*
+│➯ *.nsfwassnsfwbdsm*
+│➯ *.nsfwcum*
+│➯ *.nsfwero*
+│➯ *.nsfwfemdom*
+│➯ *.nsfwfoot*
+│➯ *.nsfwglss*
+│➯ *.nsfworgy*
+│➯ *.yuri*
+│➯ *.yuri2*
+│➯ *.yaoi*
+│➯ *.yaoi2*
+│➯ *.panties*
+│➯ *.tits*
+│➯ *.booty*
+│➯ *.pechos*
+│➯ *.tetas*
+│➯ *.booty*
+│➯ *.ecchi*
+│➯ *.furro*
+│➯ *.hentai*
+│➯ *.trapito*
+│➯ *.imagenlesbians*
+│➯ *.pene*
+│➯ *.porno*
+│➯ *.randomxxx*
+│➯⫷᭄©𝙳𝚈𝙻𝙰𝙽𝙱𝙾𝚃✍
+︎╰───────────────╯`.trim()
+if (m.isGroup) {
+//await conn.sendFile(m.chat, vn, 'Darling.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nite𝙰𝙳𝚁𝙸𝙴𝙻-𝙱𝙾𝚃-𝙼𝙳m1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })  
+} else {    
+//await conn.sendFile(m.chat, vn, 'Darling.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+let fkontak2 = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }  
+conn.sendMessage(m.chat, { image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')}, { quoted: fkontak2 })}
+} catch {
+conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
+}}
+handler.command = /^(labiblia|Labiblia|menú+18|menu+18)$/i
+handler.exp = 50
+handler.fail = null
+handler.register = true
+export default handler
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
