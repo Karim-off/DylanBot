01ced823f48bc6da4977fa68b3ad0623ef311015
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-25 19:39:22 -0400

    Actualizar grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -8,8 +8,8 @@ let pesan = args.join` `
 let oi = `*𝙈𝙚𝙣𝙨𝙖𝙟𝙚:* ${pesan}`
 let teks = `╭━┄〔 *${wm}* 〕┄⊱\n│ 𝘼𝙘𝙩𝙞𝙫𝙚𝙣𝙨𝙚 𝙥𝙡𝙖𝙣𝙩𝙖𝙨 🐹*\n│\n│❏ ${oi}\n│\n│❏ *𝙻𝙾𝚂 𝙸𝙽𝚅𝙾𝙲𝙾 𝙱𝙾𝚃𝚂:*\n`
 for (let mem of participants) {
-teks += `│🐹 @${mem.id.split('@')[0]}\n`}
-teks += `╰── @𝐃𝐲𝐥𝐚𝐧𝐁𝐨𝐭 🐹 \n\n`
+teks += `│🐻‍❄️ @${mem.id.split('@')[0]}\n`}
+teks += `╰── @𝘿𝙮𝙡𝙖𝙣𝘽𝙤𝙩-𝙈𝘿🐻‍❄️ \n\n`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true })
 }
