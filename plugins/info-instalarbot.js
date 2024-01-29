1d4ec42d87168c06574426656f7f747ebf377122
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-28 23:31:32 -0400

    Actualizar info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -2,10 +2,10 @@ import { generateWAMessageFromContent } from '@whiskeysockets/DokiBot'
 let handler  = async (m, { conn }) => {
 let texto = `
 ╭─────────────┈⊷
-│ *No seas rata bro , para comprar el bot o saber los precios comunícate con mi creador https://wa.me/522212009202*
+│ *No seas rata bro , para obtener el bot o añadir alos grupos comunícate con mi creador https://wa.me/59168683798*
 ╰┬────────────┈⊷` 
 let aa = { quoted: m, userJid: conn.user.jid }
-let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: `${texto}`.trim(), contextInfo: { externalAdReply: { title: '𝐓𝐡𝐞 𝐃𝐲𝐥𝐚𝐧 𝐁𝐨𝐭 ', body: null, thumbnail: imagen2, sourceUrl: 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33' }, mentionedJid: [m.sender] }}}, aa)
+let prep = generateWAMessageFromContent(m.chat, { extendedTextMessage: { text: `${texto}`.trim(), contextInfo: { externalAdReply: { title: '𝘿𝙮𝙡𝙖𝙣𝘽𝙤𝙩-𝙈𝘿 ', body: null, thumbnail: imagen2, sourceUrl: 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33' }, mentionedJid: [m.sender] }}}, aa)
 conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] })  
 }
 handler.tags = ['main']
