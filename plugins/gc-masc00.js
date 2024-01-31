a203f4ef87bb2d6119ba38790d887b5995027044
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-31 11:14:54 -0400

    Actualizar plugins

---
+++ plugins/gc-masc00.js
@@ -0,0 +1,41 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}
+let pesan = args.join` `
+let oi = `𝘓𝘐𝘚𝘛𝘈 𝘔𝘈𝘚𝘊❤️‍🔥•: ${pesan}`
+let teks = `╭┈┈ ๑👻๑ •• ${oi} ๑👻๑ ••:\n`
+for (let mem of participants) {
+teks += `@${mem.id.split('@')[0]}❤️‍🔥`}
+teks += `ㅤ
+│
+│         🐻‍❄️ 𝘿𝙮𝙡𝙖𝙣𝘽𝙤𝙩-𝙈𝘿 🐻‍❄️
+│        ₊˚︶︶︶︶︶︶︶︶˚ ‧₊
+│
+│      ෆ 𝘔𝘰𝘥𝘰: 500HP
+│      ෆ 𝘏𝘰𝘳𝘢𝘳𝘪𐐫:
+│         ‧₊⏱️ 00 🇵🇪 ⌇ 01 🇧🇴 ⌇ 02 🇦🇷      
+│
+│ㅤㅤʚ 𝙹𝚄𝙶𝙰𝙳𝙾𝚁𝙴𝚂❄️:
+│ㅤ🐼• 
+│ㅤ🐼• 
+│ㅤ🐼• 
+│ㅤ🐼•
+│   ㅤㅤ
+│ㅤㅤʚ 𝚂𝚄𝙿𝙻𝙴𝙽𝚃𝙴𝚂❄️:
+│ㅤ🐼•
+│ㅤ🐼•
+│
+│  ⊹ ִֶָ𝙳𝙾𝙽𝙰𝙳𝙾𝚁 𝙳𝙴 𝚂𝙰𝙻𝙰: 
+│ㅤ
+│. 🐼•
+╰───────────────๑ •`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['smsmascinfinito00 <mesaje>','mascinfinito00 <mesaje>']
+handler.tags = ['group']
+handler.command = /^(smsmascinfinito00|mascinfinito00)$/i
+handler.admin = true
+handler.group = true
+export default handler
