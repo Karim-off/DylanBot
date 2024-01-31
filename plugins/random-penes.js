2427c5309603151f73903833b82da9440dc0936c
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-01-31 19:24:55 -0400

    Actualizar random-penes.js

--- plugins/random-penes.js
+++ plugins/random-penes.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
-let url = pene[Math.floor(Math.random() * pies.length)]
+let url = pene[Math.floor(Math.random() * pene.length)]
 conn.sendFile(m.chat, url, 'error.jpg', `🥵 ♥ PENE ♥  🥵`, m)
 //conn.sendButton(m.chat, `🥵 ♥ PENE ♥  🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
