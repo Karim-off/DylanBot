dafa14cf787d0dc8393a304b15d8e93cc0c180d3
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-01 22:03:27 -0400

    Actualizar random-tetas.js

--- plugins/random-tetas.js
+++ plugins/random-tetas.js
@@ -1,1 +1,39 @@
-.
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${lenguajeGB['smsContAdult']()}`
+let url = tetas[Math.floor(Math.random() * tetas.length)]
+conn.sendFile(m.chat, url, 'error.png.jpg', `🥵 TETAS 🥵`, m)
+//conn.sendButton(m.chat, `🥵  tetas   🥵`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
+}
+handler.help = ['tetas']
+handler.tags = ['internet']
+handler.command = /^(tetas)$/
+handler.exp = 50
+handler.level = 1
+export default handler
+
+
+global.pene = [
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202787780850360411/IMG-20240201-WA0141.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202787789675171890/IMG-20240201-WA0140.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202787797199491172/IMG-20240201-WA0137.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202787819916099606/IMG-20240201-WA0138.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202787829369929788/IMG-20240201-WA0139.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202787838014259260/IMG-20240201-WA0135.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788031141126154/IMG-20240201-WA0133.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788051441680504/IMG-20240201-WA0134.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788079316762714/IMG-20240201-WA0130.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788095699845130/IMG-20240201-WA0131.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788125383065610/IMG-20240201-WA0127.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788144668221552/IMG-20240201-WA0128.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788161302827038/IMG-20240201-WA0129.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788175249014854/IMG-20240201-WA0125.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788192495861860/IMG-20240201-WA0126.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788208279158784/IMG-20240201-WA0124.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202788223143911485/IMG-20240201-WA0123.png",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202795688744976405/IMG-20240201-WA0144.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202795879527092297/IMG-20240201-WA0143.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202795917993058304/IMG-20240201-WA0148.jpg",
+"https://cdn.discordapp.com/attachments/1202339620939898981/1202795942206636114/IMG-20240201-WA0149.jpg",
+"",
+]
