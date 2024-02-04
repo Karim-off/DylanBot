54105c8f2f18539966b7fb259237b9ac3170c172
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-04 12:44:21 -0400

    Actualizar fun-gay.js

--- plugins/fun-gay.js
+++ plugins/fun-gay.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 let vn = './Plugins/Audios/gay2.mp3'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/gay', {
-avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://cdn.discordapp.com/attachments/1202339620939898981/1203742652533837835/New_Project_319_D30B0C9.png'),
 }), 'error.png', '*🏳️‍🌈 𝙼𝙸𝚁𝙴𝙽 𝙰 𝙴𝚂𝚃𝙴 𝙺𝙰𝙱𝚁𝙾 🏳️‍🌈*\n\n*🏳️‍🌈LOOK AT THIS GAY🏳️‍🌈*', m)
 await await await conn.sendFile(m.chat, vn, 'error.mp3', null, m, true, {
 type: 'audioMessage', 
