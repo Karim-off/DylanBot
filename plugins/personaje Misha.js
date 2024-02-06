2ffa5641ac04e7aca463de613965db4f0aa4a9c1
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-06 18:37:55 -0400

    Actualizar plugins

---
+++ plugins/personaje Misha.js
@@ -0,0 +1,57 @@
+let handler = async (m, { isPrems, conn }) => {
+let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
+if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+
+let img = 'https://cdn.discordapp.com/attachments/1202339620939898981/1204556224918458379/7-misha-0.jpg?ex=65d52967&is=65c2b467&hm=092f36cbd2be4fbc06e135d0334fb59052e3284ec1b48b0cc16808ae28f45449&'
+let dia = Math.floor(Math.random() * 30)
+let tok = Math.floor(Math.random() * 10)
+let hadesb = Math.floor(Math.random() * 4000)
+let expp = Math.floor(Math.random() * 5000)
+
+  global.db.data.users[m.sender].limit += dia
+  global.db.data.users[m.sender].money += hadesb
+  global.db.data.users[m.sender].joincount += tok
+  global.db.data.users[m.sender].exp += expp
+
+let texto = `*ESTE ES MISHA ✅DB*`
+let texto = `_*Habilidad especial: todos los vehículos conducidos por Misha alcanzan una mayor velocidad y reciben menos daño. Misha también recibe menos daño y es más difícil apuntarle. Habilidad pasiva.*_`
+
+const fkontak = {
+        "key": {
+    "participants":"0@s.whatsapp.net",
+                "remoteJid": "status@broadcast",
+                "fromMe": false,
+                "id": "Halo"
+        },
+        "message": {
+                "contactMessage": {
+                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                }
+        },
+        "participant": "0@s.whatsapp.net"
+}
+await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
+//await conn.sendButton(m.chat, texto, wm, img, [['🔰 𝙼𝙴𝙽𝚄', '/menu'] ], fkontak, m)  
+global.db.data.users[m.sender].lastcofre = new Date * 1
+}
+handler.help = ['daily']
+handler.tags = ['xp']
+handler.command = ['personajemisha', 'mishaff', 'mishafreefire'] 
+handler.register = true
+export default handler
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + " Horas " + minutes + " Minutos"
+}
