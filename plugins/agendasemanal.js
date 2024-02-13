fdf02f9633a9d0478de3482b4da7ddb0e34b1cdc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-02-13 15:36:41 -0400

    Actualizar agendasemanal.js

--- plugins/agendasemanal.js
+++ plugins/agendasemanal.js
@@ -2,7 +2,7 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://cdn.discordapp.com/attachments/1202339620939898981/1204534386922487839/LAAGENDASEMANALESTAAQUITraemosdeTODOparapasarlatemporadadeAmorRoyaleconDuocomoL....ElarsenaldelDesconocidollegaparaliberartudemoniointeriorylosnuevosaspectosdefebreroaterrizanjuntoc.jpg?ex=65d51511&is=65c2a011&hm=1f4aa577b0d56de7b8a65274e14b20726180e1248c14504170e853b161a503fa&'
+let img = 'https://cdn.discordapp.com/attachments/1202339620939898981/1207047394387165246/LAAGENDASEMANALESTAAQUIElAmorRoyaleestaenelaireconunnuevoemoteespecialyunnuevoa...tsdefiendelosPVdetuduoytuescuadradeamigoscontuspropiasmanosequipandoelpunoTriunfadorSupremoylak.jpg?ex=65de397c&is=65cbc47c&hm=6a06510d579316a185c522b7de2a164ad0a028f915e2f5f3fd8fcbc9fca66cd3&'
 let dia = Math.floor(Math.random() * 30)
 let tok = Math.floor(Math.random() * 10)
 let hadesb = Math.floor(Math.random() * 4000)
